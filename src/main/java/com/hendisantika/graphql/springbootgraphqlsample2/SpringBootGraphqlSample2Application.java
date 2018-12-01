e6a2b38beae3809b4bb85b482c6d1d319291f8fb
Hendi Santika <hendisantika@gmail.com>
2018-12-01 09:19:49 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/graphql/springbootgraphqlsample2/SpringBootGraphqlSample2Application.java
@@ -0,0 +1,12 @@
+package com.hendisantika.graphql.springbootgraphqlsample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlSample2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGraphqlSample2Application.class, args);
+    }
+}
---
+++ src/test/java/com/hendisantika/graphql/springbootgraphqlsample2/SpringBootGraphqlSample2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.graphql.springbootgraphqlsample2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootGraphqlSample2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
